cdc49b9d22b31aa4c8c8a74a12f21bb049c78eb2
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-08 15:20:23 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/scrollup-master/trunk@871878 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+Contributors: sayful
+Tags:  arrow, navigation, scroll, scroll up, top, scrollup-master, back to top
+Requires at least: 2.9.2
+Tested up to: 3.8.1
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is just a very simple plugin to have a unified "scroll to top" button throughout your whole blog. Juste install the plugin a "scroll to top" button will appear at right bottom of your blog.
+
+== Description ==
+
+Fully written in jQuery, the plugin adds a floating navigation button right bottom in the footer of your site that only appears when you scroll the page down, and when clicked gently roll the site to the top. All this without any modification to your template.
+
+== Installation ==
+
+1.Extract the zip file and just drop the contents in the wp-content/plugins/ directory.
+2.Activate the Plugin from Plugins page.
+
+== Screenshots ==
+
+1. Screenshot-1
+1. Screenshot-2
+
+== Changelog ==
+
+= version 1.0 =
+Implementation of basic functionality.
+
+== CREDIT ==
+
+1.This plugin was developed by Sayful Islam - http://sis.netai.net
+2.Some open source framework have been used. For detail visit at https://github.com/markgoodyear/scrollup
+
+== CONTACT ==
+
+Rafael Cirolini
+http://www.sis.netai.net
+http://www.sayful1.wordpress.com
+
+
+
+
---
+++ scrollup.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Scrollup-master
+Plugin URI: http://wordpress.org/plugins/scrollup-master/
+Description: This plugin is for wordpress to create scroll to top button.
+Version: 1.0.0
+Author: Sayful Islam
+Author URI: http://sayful1.wordpress.com
+License: GPLv2 or later
+*/
+
+ /* Adding Latest jQuery from Wordpress */
+function scrollup_plugin_scripts() {
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('scrool_up_custom_scrollUp',plugins_url( '/js/jquery.scrollUp.min.js' , __FILE__ ),array( 'jquery' ));
+	wp_enqueue_script('scrool_up_custom_easing',plugins_url( '/js/jquery.easing.min.js' , __FILE__ ),array( 'jquery' ));
+	wp_enqueue_script('scrool_up_tooltip_plugin',plugins_url( '/js/tooltip.js' , __FILE__ ),array( 'jquery' ));
+
+	wp_enqueue_style('scrool_up_custom_style',plugins_url( '/css/scrollup.css' , __FILE__ ));
+	wp_enqueue_style('scrool_up_tooltip_style',plugins_url( '/css/tooltip.css' , __FILE__ ));
+
+}
+add_action('init', 'scrollup_plugin_scripts');
+
+
+function scrollup_active_hook() {
+
+	?><script type="text/javascript">
+		jQuery(function () {
+			jQuery.scrollUp({
+		        scrollName: 'scrollUp', // Element ID
+		        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+		        scrollFrom: 'top', // 'top' or 'bottom'
+		        scrollSpeed: 700, // Speed back to top (ms)
+		        easingType: 'easeInExpo', // Scroll to top easing (see http://easings.net/)
+		        animation: 'fade', // Fade, slide, none
+		        animationInSpeed: 200, // Animation in speed (ms)
+		        animationOutSpeed: 200, // Animation out speed (ms)
+		        scrollText: 'Scroll to top', // Text for element, can contain HTML
+		        scrollTitle: 'Scroll to top', // Set a custom <a> title if required. Defaults to scrollText
+		        scrollImg: true, // Set true to use image
+		        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+		        zIndex: 2147483647 // Z-Index for the overlay
+			});
+		});
+	</script><?php
+
+}
+add_action('wp_footer', 'scrollup_active_hook');
+function scrollup_tooltip_active_hook() {
+
+	?><script type="text/javascript">
+jQuery(function(){
+    jQuery("#scrollUp").tooltip({
+        title 	  	: 'It works in absence of title attribute.',
+        animation 	: true,
+        placement 	: 'auto', // top | bottom | left | right | auto.
+        trigger		: 'hover',	//click | hover | focus | manual.
+        delay		: { show: 400, hide: 100 }
+    });
+});
+	</script><?php
+
+}
+add_action('wp_footer', 'scrollup_tooltip_active_hook');
+?>
