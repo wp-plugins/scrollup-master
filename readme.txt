e582670ae1071c141f29ebc2d0c888d1c5a23c5f
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-16 18:21:03 +0000

    Update version of plugins
    git-svn-id: https://plugins.svn.wordpress.org/scrollup-master/trunk@876310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 === scrollup-master ===
-Contributors: sayful Islam
+Contributors: sayful
 Tags:  arrow, navigation, scroll, scroll up, top, scrollup-master, back to top
 Requires at least: 2.9.2
 Tested up to: 3.8.1
@@ -15,24 +15,37 @@ Fully written in jQuery, the plugin adds a floating navigation button right bott
 
 == Installation ==
 
-1.Extract the zip file and just drop the contents in the wp-content/plugins/ directory.
+* Extract the zip file and just drop the contents in the wp-content/plugins/ directory.
 
-2.Activate the Plugin from Plugins page.
+* Activate the Plugin from Plugins page.
+
+* You will find 'scrollup' menu in your WordPress Settings panel.
+
+== Frequently Asked Questions ==
+Do you have questions or issues with scrollup-master. You can ask [here](http://sis.netai.net)
 
 == Screenshots ==
 
 1. Screenshot-1
 1. Screenshot-2
+1. Screenshot-3
 
 == Changelog ==
+
+= version 2.0 =
+1.More lighter than previous version.
+
+2.New option page has been included where you can change default easing effect, scroll speed, scroll distance, hover title, button animation type.
 
 = version 1.0 =
 Implementation of basic functionality.
 
+== Upgrade Notice ==
+You should upgrade the plugin to get more function and faster speed.
+
 == CREDIT ==
 
 1.This plugin was developed by [Sayful Islam](http://sis.netai.net)
-
 2.Some open source framework have been used. For detail visit [here](https://github.com/markgoodyear/scrollup)
 
 == CONTACT ==
--- scrollup.php
+++ scrollup.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Scrollup-master
+Plugin Name: scrollup-master
 Plugin URI: http://wordpress.org/plugins/scrollup-master/
 Description: This plugin is for wordpress to create scroll to top button.
-Version: 1.0.0
+Version: 2.0
 Author: Sayful Islam
 Author URI: http://sayful1.wordpress.com
 License: GPLv2 or later
@@ -27,15 +27,15 @@ function scrollup_active_hook() {
 		jQuery(function () {
 			jQuery.scrollUp({
 		        scrollName: 'scrollUp', // Element ID
-		        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+		        scrollDistance: <?php echo strlen(trim(get_option("scrollup_master_scrolldistance")))>0?trim(get_option("scrollup_master_scrolldistance")):'300'?>, // Distance from top/bottom before showing element (px)
 		        scrollFrom: 'top', // 'top' or 'bottom'
-		        scrollSpeed: 700, // Speed back to top (ms)
-		        easingType: 'easeInExpo', // Scroll to top easing (see http://easings.net/)
-		        animation: 'fade', // Fade, slide, none
+		        scrollSpeed: <?php echo strlen(trim(get_option("scrollup_master_scrollspeed")))>0?trim(get_option("scrollup_master_scrollspeed")):'300'?>, // Speed back to top (ms)
+		        easingType: '<?php echo strlen(trim(get_option("scrollup_master_tooltipeasingtype")))>0?trim(get_option("scrollup_master_tooltipeasingtype")):'linear'?>',
+		        animation: '<?php echo strlen(trim(get_option("scrollup_master_animation")))>0?trim(get_option("scrollup_master_animation")):'fade'?>', // Fade, slide, none
 		        animationInSpeed: 200, // Animation in speed (ms)
 		        animationOutSpeed: 200, // Animation out speed (ms)
 		        scrollText: 'Scroll to top', // Text for element, can contain HTML
-		        scrollTitle: 'Scroll to top', // Set a custom <a> title if required. Defaults to scrollText
+		        scrollTitle: '<?php echo strlen(trim(get_option("scrollup_master_tooltiptitle")))>0?trim(get_option("scrollup_master_tooltiptitle")):'Scroll to top'?>',
 		        scrollImg: true, // Set true to use image
 		        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 		        zIndex: 2147483647 // Z-Index for the overlay
@@ -46,5 +46,30 @@ function scrollup_active_hook() {
 }
 add_action('wp_footer', 'scrollup_active_hook');
 
-?>
+/*Scrollup Option Page*/
+function register_scrollup_custom_option_page(){
+	//create new settings menu
+    add_options_page( 'Scrollup-master Settings', 'scrollup-master', 'manage_options', 'scrollup-master/admin.php', '', 6 );
+}
+add_action( 'admin_menu', 'register_scrollup_custom_option_page' );
 
+//register our settings.
+function register_scrollup_settings(){
+	register_setting('scrollup-master-settings-group','scrollup_master_tooltiptitle');
+	register_setting('scrollup-master-settings-group','scrollup_master_tooltipeasingtype');
+	register_setting('scrollup-master-settings-group','scrollup_master_scrollspeed');
+	register_setting('scrollup-master-settings-group','scrollup_master_scrolldistance');
+	register_setting('scrollup-master-settings-group','scrollup_master_animation');
+}
+add_action( 'admin_init', 'register_scrollup_settings' );
+
+// function to be run when the plugin is activated.
+function scrollup_master_activation() {
+	add_option('scrollup_master_tooltiptitle','Scroll to top');
+	add_option('scrollup_master_tooltipeasingtype','linear');
+	add_option('scrollup_master_scrollspeed',300);
+	add_option('scrollup_master_scrolldistance',300);
+	add_option('scrollup_master_animation','fade');
+}
+register_activation_hook( __FILE__, 'scrollup_master_activation' );
+?>
